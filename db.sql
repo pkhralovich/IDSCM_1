3be7329357af0f960065fb2fc331d55b7cc0da9f
Pavel Khralovich <pkhralovich@gmail.com>
2021-03-03 14:50:00 +0100

    First commit

---
+++ db.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `idscm`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `surname` VARCHAR(45),
+  `email` VARCHAR(45) NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `username` (`username` ASC) VISIBLE,
+  UNIQUE INDEX `email` (`email` ASC) VISIBLE);
+
+CREATE TABLE `idscm`.`videos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NOT NULL,
+  `author` VARCHAR(45) NULL,
+  `creation_date` DATETIME NOT NULL,
+  `duration` INT NOT NULL DEFAULT 0,
+  `plays` INT NOT NULL DEFAULT 0,
+  `description` VARCHAR(45) NOT NULL,
+  `format` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
