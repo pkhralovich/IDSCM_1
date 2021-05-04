82d8b9e75465f0e2eb34675d332b6efb2753f085
Cristian Matas <cristian.matas@gmail.com>
2021-05-04 20:08:44 +0200

    started security

--- src/main/java/com/upc/idscm/tools/Encryption.java
+++ src/main/java/com/upc/idscm/tools/Encryption.java
@@ -1,7 +1,9 @@
 package com.upc.idscm.tools;
 
 import com.google.common.hash.Hashing;
+import com.sun.org.apache.xml.internal.security.encryption.XMLCipher;
 import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
 
 public class Encryption {
     public static String hash(String value) {
@@ -9,5 +11,14 @@ public class Encryption {
                         .hashString(value, StandardCharsets.UTF_8)
                         .toString();
         return result;
+    }
+    
+    public static String encrypt_xml_file(XML xml_file) {
+        XMLCipher keyCipher = XMLCipher.getInstance(XMLCipher.AES_128);
+        SecretKey symmetricKey = new SecretKeySpec(key.getBytes(), "AES");
+        xmlCipher.init(XMLCipher.ENCRYPT_MODE, symmetricKey);
+        boolean encryptContentsOnly = false;
+        xmlCipher.doFinal(node.getOwnerDocument(), (Element)node,
+        encryptContentsOnly);
     }
 }
